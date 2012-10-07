83b30cdb204a73cb0d982fa953814004d101c1da
Martin Ueding <dev@martin-ueding.de>
2012-10-07 17:35:35 +0200

    Add PDF source move script here

---
+++ move_PDF_Source.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2012 Martin Ueding <dev@martin-ueding.de>
+
+set -e
+set -u
+
+shopt -s nullglob
+
+cd "$HOME/PDF_Source"
+
+for c in {A..Z} {a..z} {0..9}
+do
+	files=$(echo ${c}*.*)
+
+	if [[ -n "$files" ]]
+	then
+		echo "Folder $c"
+
+		if [[ ! -d $c ]]
+		then
+			mkdir $c
+		fi
+
+		mv $files $c/
+	fi
+done
