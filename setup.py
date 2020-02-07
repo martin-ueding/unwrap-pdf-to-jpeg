43c83db95a1c0d43c0ad520f42b670730c6e917b
Martin Ueding <no-reply@martin-ueding.de>
2020-02-07 11:35:20 +0100

    Remove now discontinued email address

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright © 2012-2013 Martin Ueding <dev@martin-ueding.de>
+# Copyright © 2012-2013 Martin Ueding <martin-ueding.de>
 
 from distutils.core import setup
 
 setup(
     author = "Martin Ueding",
-    author_email = "dev@martin-ueding.de",
     description = "Converts JPEG files to PDF and back",
     license = "GPL2+",
     name = "wrap-jpeg-to-pdf",
